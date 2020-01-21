63baf28ffef831ee70f71c8fd48510e71b124b90
Robert Quentin <rasquent136@gmail.com>
2020-01-22 00:22:43 +0100

    Add files via upload

---
+++ about.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Une petite biographie de Robert Quentin, depuis ma naissance jusque maintenant.">
+    <meta name="keywords" content="robert quentin, robert quentin portfolio, robert quentin développeur web, robert quentin web, robert quentin biographie">
+    <script src="https://kit.fontawesome.com/6a7d53791c.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/main.css">
+    <title>Document</title>
+  </head>
+  <body>
+    <nav class="main_nav">
+      <a href="index.php">
+        <h1 class="logo_title">RQ</h1>
+      </a>
+      <ul>
+        <li><a class="current_page" href="about.php">Présentation</a></li>
+        <li><a href="projects.php">Projets</a></li>
+        <li><a href="contact.php">Me Contacter</a></li>
+      </ul>
+    </nav>
+
+    <section class="about_section text_center ptb_3">
+      <div class="container">
+        <h2 class="section_title">Biographie</h2>
+        <p class="section_text">Développeur web & web mobile ( formation )</p>
+        <div class="about_info ptb_2">
+          <img src="img/rasquent.jpeg" alt="Image perso pour biographie" class="bio_img">
+          <div class="bio_content">
+            <h4 class="about_title">Petite description </h4>
+            <ul>
+              <li>Jeune développeur web, en formation dans les grandes écoles du numérique ( UTOPLAB/SIMPLON ), je maitrise de plus en plus les languages suivant : HTML/CSS, Javascript, Sass et Php. Mon amour pour mes passions, ( informatique, musique, l'écriture, jeux vidéo, astronomie )  m'ont amené à les relier, pour en faire de grandes choses, enfin je l'espère . Merci de votre compréhension et n'hésitez pas à me contacter.  </li>
+              <a href="img/Quentin_Robert_CV.pdf" class="btn btn_cta">Télécharger mon CV</a>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li><i class="far fa-folder-open fa-4x"></i></li>
+              <li class="about_icon_title">Projets | 4</li>
+              <li>Voiçi mes premiers projets ...</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li><i class="far fa-user fa-4x"></i></li>
+              <li class="about_icon_title">Clients | 2</li>
+              <li>J'ai également travaillez pour des clients.</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li><i class="fas fa-star fa-4x"></i></li>
+              <li class="about_icon_title">Note moyenne | 3/5</li>
+              <li>Voiçi les premiers compte rendu de mon travail.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="about_section_technologies ptb_3">
+      <div class="container">
+        <h2 class="section_title text_center">Mes technologies</h2>
+        <p class="section_text text_center">Ce sont les technologies sur lesquels j'ai le plus travailler .</p>
+        <h3><img src="img/html.png "  height="75px"></h3>
+        <div class="progress_bar">
+          <div class="progress_bar_50"></div>
+        </div>
+        <h3><img src="img/CSS.png"  height="75px"></h3>
+        <div class="progress_bar">
+          <div class="progress_bar_50"></div>
+        </div>
+        <h3><img src="img/js.png"  height="75px"></h3>
+        <div class="progress_bar">
+          <div class="progress_bar_40"></div>
+        </div>
+        <h3><img src="img/sass.jpeg"  height="75px"></h3>
+        <div class="progress_bar">
+          <div class="progress_bar_50"></div>
+        </div>
+        <h3><img src="img/php.png"  height="75px"></h3>
+        <div class="progress_bar">
+          <div class="progress_bar_40"></div>
+        </div>
+        <h3><img src="img/balsamiq.png"  height="75px"></h3>
+        <div class="progress_bar">
+          <div class="progress_bar_60"></div>
+        </div>
+        <h3><img src="img/wordpress-com-org.jpg"  height="75px"></h3>
+        <div class="progress_bar">
+          <div class="progress_bar_60"></div>
+        </div>
+        <h3><img src="img/boostrap.jpeg"  height="75px"></h3>
+        <div class="progress_bar">
+          <div class="progress_bar_60"></div>
+        </div>
+        <h3><img src="img/vueJS.jpeg"  height="75px"></h3>
+        <div class="progress_bar">
+          <div class="progress_bar_50"></div>
+        </div>
+        <h3><img src="img/nodeJS.png"  height="75px"></h3>
+        <div class="progress_bar">
+          <div class="progress_bar_50"></div>
+        </div>
+        <hr class="section_separation">
+        <div class="technologies text_center">
+          <div>
+            <i class="fas  fa-2x"><img src="img/html.png" height="50px"></i>
+            <h3>HTML</h3>
+            <p>Semi-langage incontournable pour tout développeur web, le HTML est à la base de tout site internet.</p>
+          </div>
+          <div>
+            <i class="fas  fa-2x"><img src="img/CSS.png" height="50px"></i>
+            <h3>CSS</h3>
+            <p>Le Css, est le complément au HTML ! Avec, on laisse déborder notre imagination dans nos projets .</p>
+          </div>
+          <div>
+            <i class="fas  fa-2x"><img src="img/js.png" height="50px"></i>
+            <h3>JAVASCRIPT</h3>
+            <p>Avec le Javascript , internet s'illumine d'animation ! </p>
+          </div>
+          <div>
+            <i class="fas  fa-2x"><img src="img/php.png" height="50px"></i>
+            <h3>PHP</h3>
+            <p>Langage incontournable pour le font-end ! </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer class="main_footer ptb_3">
+      <div class="footer_content container text_center">
+        <p>Copyright &copy; 2020 Robert Quentin. Tous droits réservés. <span class="contact_footer">Email: <a href="mailto:rasquent136@gmail.com">rasquent136@gmail.com</a> | Tel: <a href="tel:+636664943">+636664943</a></span></p>
+      </div>
+    </footer>
+  </body>
+</html>
---
+++ projects.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Tous les projets de robert quentin  regroupez en une seule page. N'hésitez pas à me contacter pour un partenariat.">
+    <meta name="keywords" content="robert quentin, robert quentin portfolio, robert quentin développeur web, robert quentin web, robert quentin projets">
+    <script src="https://kit.fontawesome.com/6a7d53791c.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/main.css">
+    <title>Document</title>
+  </head>
+  <body>
+    <nav class="main_nav">
+      <a href="index.php">
+        <h1 class="logo_title">RQ</h1>
+      </a>
+      <ul>
+        <li><a href="about.php">Présentation</a></li>
+        <li><a class="current_page" href="projects.php">Projets</a></li>
+        <li><a href="contact.php">Me Contacter</a></li>
+      </ul>
+    </nav>
+
+    <section class="projects_section text_center ptb_3">
+      <h2 class="section_title">Mes projets</h2>
+      <p class="section_text">Je vous laisse découvrir mes projets personnel.</p>
+      <div class="container">
+        <div class="projects ptb_2">
+          <div class="project">
+            <img src="img/siteCv.png" alt="Placeholder" height="180">
+            <div class="project_content">
+              <p class="project_text">Voiçi mon site CV personnel. J'ai commencé à m'exercé avec les langages HTML/CSS ainsi que le PHP.</p>
+              <a href="https://rasquentcv.000webhostapp.com/" class="btn btn_project_section"><i class="fas fa-eye"></i>Démo</a>
+              <a href="https://github.com/rasQuent136/cvPerso" class="btn btn_project_section"><i class="fas fa-code"></i>Github</a>
+            </div>
+          </div>
+          <div class="project">
+            <img src="img/formulaire_javascript.png" alt="Placeholder" height="180">
+            <div class="project_content">
+              <p class="project_text">Mon premier formulaire en JAVASCRIPT. Simple, mais avec quelques petites contraite dans l'exercice que l'on devait rendre.</p>
+              <a href="#" class="btn btn_project_section"><i class="fas fa-eye"></i>Démo</a>
+              <a href="https://github.com/rasQuent136/Formullaire" class="btn btn_project_section"><i class="fas fa-code"></i>Github</a>
+            </div>
+          </div>
+          <div class="project">
+            <img src="https://via.placeholder.com/320x180" alt="Placeholder">
+            <div class="project_content">
+              <p class="project_text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta autem illo dolorum. At voluptate quae commodi! Deleniti est quo harum labore quisquam a qui doloremque, nisi odio quas aperiam repellendus?</p>
+              <a href="#" class="btn btn_project_section"><i class="fas fa-eye"></i>Démo</a>
+              <a href="#" class="btn btn_project_section"><i class="fas fa-code"></i>Github</a>
+            </div>
+          </div>
+          <div class="project">
+            <img src="https://via.placeholder.com/320x180" alt="Placeholder">
+            <div class="project_content">
+              <p class="project_text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta autem illo dolorum. At voluptate quae commodi! Deleniti est quo harum labore quisquam a qui doloremque, nisi odio quas aperiam repellendus?</p>
+              <a href="#" class="btn btn_project_section"><i class="fas fa-eye"></i>Démo</a>
+              <a href="#" class="btn btn_project_section"><i class="fas fa-code"></i>Github</a>
+            </div>
+          </div>
+          <div class="project">
+            <img src="https://via.placeholder.com/320x180" alt="Placeholder">
+            <div class="project_content">
+              <p class="project_text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta autem illo dolorum. At voluptate quae commodi! Deleniti est quo harum labore quisquam a qui doloremque, nisi odio quas aperiam repellendus?</p>
+              <a href="#" class="btn btn_project_section"><i class="fas fa-eye"></i>Démo</a>
+              <a href="#" class="btn btn_project_section"><i class="fas fa-code"></i>Github</a>
+            </div>
+          </div>
+          <div class="project">
+            <img src="https://via.placeholder.com/320x180" alt="Placeholder">
+            <div class="project_content">
+              <p class="project_text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta autem illo dolorum. At voluptate quae commodi! Deleniti est quo harum labore quisquam a qui doloremque, nisi odio quas aperiam repellendus?</p>
+              <a href="#" class="btn btn_project_section"><i class="fas fa-eye"></i>Démo</a>
+              <a href="#" class="btn btn_project_section"><i class="fas fa-code"></i>Github</a>
+            </div>
+          </div>
+          <div class="project">
+            <img src="https://via.placeholder.com/320x180" alt="Placeholder">
+            <div class="project_content">
+              <p class="project_text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta autem illo dolorum. At voluptate quae commodi! Deleniti est quo harum labore quisquam a qui doloremque, nisi odio quas aperiam repellendus?</p>
+              <a href="#" class="btn btn_project_section"><i class="fas fa-eye"></i>Démo</a>
+              <a href="#" class="btn btn_project_section"><i class="fas fa-code"></i>Github</a>
+            </div>
+          </div>
+          <div class="project">
+            <img src="https://via.placeholder.com/320x180" alt="Placeholder">
+            <div class="project_content">
+              <p class="project_text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta autem illo dolorum. At voluptate quae commodi! Deleniti est quo harum labore quisquam a qui doloremque, nisi odio quas aperiam repellendus?</p>
+              <a href="#" class="btn btn_project_section"><i class="fas fa-eye"></i>Démo</a>
+              <a href="#" class="btn btn_project_section"><i class="fas fa-code"></i>Github</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer class="main_footer ptb_3">
+      <div class="footer_content container text_center">
+        <p>Copyright &copy; 2020 Robert Quentin. Tous droits réservés. <span class="contact_footer">Email: <a href="mailto:rasquent136@gmail.com">rasquent136@gmail.com</a> | Tel: <a href="tel:+636664943">+636664943</a></span></p>
+      </div>
+    </footer>
+  </body>
+</html>
